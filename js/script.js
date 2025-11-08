90f4513b27a93ba2feacc4c6e86dc75c8f0f7283
Shrejaltambe18 <tambesejal18@gmail.com>
2025-11-08 22:24:05 +0530

    next() function

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const images = ["./images/virat.png", "./images/abhishek.png", "./images/ishan.png", "./images/rohit.png", "./images/shubhman.png", "./images/shreyas.png", "./images/ravindra.png"]
+
+let index = 0;
+const slide = document.getElementById("slide");
+
+function next() {
+    if (index < images.length - 1) {
+        index++;
+    }
+    else {
+        index = 0;
+    }
+    slide.src = images[index];
+}
